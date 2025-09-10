4031c8f580d6c88a01a9f370b17227a10dbd15ca
BLLALL <bamer8353@gmail.com>
2025-09-10 12:54:52 +0300

    Restarting the cursed 2015 docs

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mage-os/magento-cache-clean' => array(
+            'pretty_version' => '1.1.2',
+            'version' => '1.1.2.0',
+            'reference' => 'efe36fc20dd8f367b4973a4538201c31fcc0182e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mage-os/magento-cache-clean',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
